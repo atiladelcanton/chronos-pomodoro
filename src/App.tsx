6818af8ef8b629c730319693b7376ad027cda2e7
Atila Delcanton Rampazo <atilarampazo@gmail.com>
2025-07-04 17:24:32 -0300

    First pull

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
+import { Menu } from './components/Menu';
+
+import './styles/theme.css';
+import './styles/global.css';
+import { CountDown } from './components/CountDown';
+
+function App() {
+  return (
+    <>
+      <Container>
+        <Logo />
+      </Container>
+      <Container>
+        <Menu />
+      </Container>
+      <Container>
+        <CountDown />
+      </Container>
+    </>
+  );
+}
+export default App;
