9f78590c73aa66192d2937f551a7c7865fe6ab98
Atila Delcanton Rampazo <atilarampazo@gmail.com>
2025-07-18 16:33:55 -0300

    Finish Project

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import './styles/global.css';
 import { TaskContextProvider } from './contexts/TaskContext/TaskContextProvider';
 import { MessagesContainer } from './components/MessagesContainer';
 import { MainRouter } from './routers/MainRouers';
-
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
   return (
     <TaskContextProvider>
       <MessagesContainer>
         <MainRouter />
+        <SpeedInsights />
       </MessagesContainer>
     </TaskContextProvider>
   );
